2387a36029200737a9acb02c32577968a628858a
wilsonbarros1 <wilsonbarros@edu.unifor.br>
2023-11-27 10:36:38 -0300

    Menu de opções e subtração
    Menu de opções e subtração

--- trabalho/Calculadora.java
+++ trabalho/Calculadora.java
@@ -6,10 +6,49 @@ public class Calculadora {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Menu de escolher operações
+        System.out.println("Escolha a operação desejada:");
+        System.out.println("1 - Soma");
+        System.out.println("2 - Subtração");
+        System.out.println("3 - Multiplicação");
+        System.out.println("4 - Divisão");
         
-        
+        // Obtém a escolha do usuário
+        System.out.print("Digite o número da operação desejada: ");
+        int escolha = scanner.nextInt();
 
-        // Solicita o primeiro número do cálculo
+            // Variáveis para armazenar os números inseridos pelo usuário
+            int numero1, numero2, resultado;
+
+            switch (escolha) {
+                case 1: // Soma
+                    System.out.println("Digite o primeiro número: ");
+                    numero1 = scanner.nextInt();
+                    System.out.println("Digite o segundo número: ");
+                    numero2 = scanner.nextInt();
+                    resultado = numero1 + numero2;
+                    System.out.printf("A soma entre %d e %d é igual a %d.%n", numero1, numero2, resultado);
+                    break;
+    
+                case 2: // Subtração
+                    System.out.println("Digite o primeiro número: ");
+                    numero1 = scanner.nextInt();
+                    System.out.println("Digite o segundo número: ");
+                    numero2 = scanner.nextInt();
+                    resultado = numero1 - numero2;
+                    System.out.printf("A subtração entre %d e %d é igual a %d.%n", numero1, numero2, resultado);
+                    break;
+    
+                // Adicione casos para multiplicação (3) e divisão (4) aqui
+    
+                default:
+                    System.out.println("Operação inválida. Por favor, escolha um número de 1 a 4.");
+            }
+    
+            scanner.close();
+        }
+
+        /*  Solicita o primeiro número do cálculo
         System.out.println("Digite o primeiro número da soma: ");
         int numero1 = scanner.nextInt();
         
@@ -27,7 +66,7 @@ public class Calculadora {
 
 
         scanner.close();
-    }
+    }*/
 
 
 }
