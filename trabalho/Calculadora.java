3927d03492d34235838ecbd4619a97f92e7bc8ce
israel-mota7 <151981329+israel-mota7@users.noreply.github.com>
2023-11-28 22:34:50 -0300

    Israel
    com2

--- trabalho/Calculadora.java
+++ trabalho/Calculadora.java
@@ -57,26 +57,6 @@ public class Calculadora {
             scanner.close();
         }
 
-        /*  Solicita o primeiro número do cálculo
-        System.out.println("Digite o primeiro número da soma: ");
-        int numero1 = scanner.nextInt();
-        
-        // Solicita o segundo número do cálculo
-        System.out.println("Digite o segundo número da soma: ");
-        int numero2 = scanner.nextInt();
-        
-        // Especifica a operação de soma que será executada
-        int soma = numero1 + numero2;
-
-        // Imprime o resultado da soma
-        System.out.printf("A soma entre %d e %d é igual a %d.", numero1, numero2 ,soma);
-
-
-
-
-        scanner.close();
-    }*/
-
 
 }
     
